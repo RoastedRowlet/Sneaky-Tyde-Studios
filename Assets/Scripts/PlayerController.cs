1cc45b96775eed01b6d655526d0d90c3b31fd138
RoastedRowlet <34221594+RoastedRowlet@users.noreply.github.com>
2018-01-07 13:15:22 -0500

    Merge branch 'Sneaky-Alex'

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D> ();
+        anim = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        if (moveHorizontal > 0)
+        {
+            moveHorizontal = 1;
+        }
+        else if(moveHorizontal < 0)
+        {
+            moveHorizontal = -1;
+        }
+
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+
+        rb2d.AddForce(movement * speed);
+
+        if(Input.GetKeyDown("d"))
+        {
+            anim.SetTrigger("Walk");
+        }
+        else
+        {
+            anim.SetTrigger("Knight1_Idle");
+        }
+       
+
+    }
+   
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+           // Destroy(other.gameObject);  <-- Probably more what we're looking for.
+            other.gameObject.SetActive(false);
+
+        }
+    }
+}
