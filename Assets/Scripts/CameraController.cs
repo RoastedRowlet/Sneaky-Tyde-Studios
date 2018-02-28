32c29e656bc2855e7b50dac1712f48de5cb6c4f5
RoastedRowlet <34221594+RoastedRowlet@users.noreply.github.com>
2018-02-27 20:03:15 -0500

    LASOR BEAMS!
    added lasers when you left click

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,24 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CameraController : MonoBehaviour {
 
     public GameObject player;
+    public float x;
 
+    [SerializeField]
     private Vector3 offset;
 
-	// Use this for initialization
-	void Start () {
+    [SerializeField]
+    Tilemap tilemap;
+
+    private float tileOverHang = 0.10f;
+
+    // Use this for initialization
+    void Start () {
 
         offset = transform.position - player.transform.position;
+        tilemap.CompressBounds();
 
-	}
+    }
 	
 	// Update is called once per frame
 	void LateUpdate () {
 
-        transform.position = player.transform.position + offset;
-		
-	}
+        Vector3 newPos = player.transform.position + offset;
+
+        Camera cam = Camera.main;
+        Vector3 tmSize = tilemap.size;
+        float camHeight = cam.orthographicSize;
+        float camWidth = camHeight * cam.aspect;
+
+        if (newPos.x + camWidth > tmSize.x/2)
+        {
+            newPos.x = tmSize.x / 2 - camWidth;
+        }
+        else if (newPos.x - camWidth < -tmSize.x / 2)
+        {
+            newPos.x = -tmSize.x / 2 + camWidth;
+        }
+
+        if (newPos.y + camHeight > tmSize.y / 2)
+        {
+            newPos.y = tmSize.y / 2 - camHeight;
+        }
+        else if (newPos.y - camHeight < -tmSize.y / 2)
+        {
+            newPos.y = -tmSize.y / 2 + camHeight;
+        }
+        transform.position = newPos;
+    }
 }
---
+++ Assets/Scripts/Spell.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spell : MonoBehaviour {
+
+    private Rigidbody2D spellRigidBody;
+
+    [SerializeField]
+    private float speed;
+
+    private Vector2 direction;
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        Camera camera = Camera.main;
+
+        spellRigidBody = GetComponent<Rigidbody2D>();
+
+        Vector3 mouseLocation = (Vector3)camera.ScreenToWorldPoint(Input.mousePosition);
+
+        direction = mouseLocation - transform.position;
+        spellRigidBody.velocity = direction.normalized * speed;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+	}
+
+    public void Fire()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
